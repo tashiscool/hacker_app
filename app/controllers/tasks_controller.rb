f8eb7a4a721d56fa5b845e2362fb8b79e18eb40a
tash <tashdid.khan@pearson.com>
2012-08-28 00:17:25 -0400

    adding pg

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,12 +28,14 @@ class TasksController < ApplicationController
    def create_sms
     @list = List.find(1)
     @ticket = params[:ticket]
-    url = URI.parse('https://sso.rumba.pearsoncmg.com/sso/login?service=http://nameless-bayou-1430.herokuapp.com/addTask&ticket=' + @ticket)
-    req = Net::HTTP::Get.new(url.path)
-    res = Net::HTTP.start(url.host, url.port) {|http|
+    if @ticket != nil
+      url = URI.parse('https://sso.rumba.pearsoncmg.com/sso/login?service=http://nameless-bayou-1430.herokuapp.com/addTask&ticket=' + @ticket)
+      req = Net::HTTP::Get.new(url.path)
+      res = Net::HTTP.start(url.host, url.port) {|http|
       http.request(req)
-    }
-    @task = @list.tasks.new(:new => res.body)
+      }
+      @task = @list.tasks.new(:new => res.body)
+    end
     if @task.save
         render :content_type  => "text/xml", :text => "<Response><Sms>Thanks for the memories</Sms></Response>"
     else
